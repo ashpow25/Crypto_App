41f3d8d72f549cdb068099dc3754779716936e7a
Ashley <ashpow25@gmail.com>
2021-08-07 18:41:49 -0700

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('loaded')
+var cors = require('cors')
+
+app.use(cors())
+
+const Start_URL = 'https://www.cryptingup.com/api/markets'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    //callback listener that fetches the cryptos & when the dom loads, displays automatically
+getPrices()
+})
+
+function getPrices(){
+    fetch('https://www.cryptingup.com/api/markets')
+    .then(response => response.json())
+    .then (price => console.log(price))
+
+        
+    
+
+    console.log(fetch('https://www.cryptingup.com/api/markets'))
+}

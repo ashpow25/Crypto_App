71a8e717a2e13acaa354423c564518361eb82570
Ashley <ashpow25@gmail.com>
2021-08-08 16:23:54 -0700

    can API into DOM

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const START_URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h`
+
+const START_URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h`
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -8,12 +9,24 @@ getPrices()
 
 function getPrices(){
     //get prices from Coingecko API
-    fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h`)
-    .then(response => response.json())
-    .then (price => console.log(price))
+    let info = document.getElementById('info ul')
+    let coinList = document.getElementById('coin-info')
+    let catalog = document.getElementById('catalog')
+    fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h`)
+        .then(response => response.json())
+        .then (coins => {
+            coins.map(coin => {
+                catalog.innerHTML += `
+                <li> 
+                   ${coin.name} 
+                </li>`
+           
+        })
+
+    })
 
         
     
 
-    console.log(fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h`))
+    console.log(fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h`))
 }

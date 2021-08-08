36537849230cdc86554805110605c3b63ec000be
Ashley <ashpow25@gmail.com>
2021-08-07 21:25:37 -0700

    API connected

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-var cors = require('cors')
-
-app.use(cors())
-
-const Start_URL = 'https://www.cryptingup.com/api/markets'
+const START_URL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h`
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    //callback listener that fetches the cryptos & when the dom loads, displays automatically
+    //callback listener that fetches the cryptos & when the DOM loads, displays automatically
 getPrices()
 })
 
 function getPrices(){
-    fetch('https://www.cryptingup.com/api/markets')
+    //get prices from Coingecko API
+    fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h`)
     .then(response => response.json())
     .then (price => console.log(price))
 
         
     
 
-    console.log(fetch('https://www.cryptingup.com/api/markets'))
+    console.log(fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=24h`))
 }
